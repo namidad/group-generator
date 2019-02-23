38261e82c24fe2ef7bbbbbbeb763b2024470c777
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-23 16:00:04 +0100

    Add files via upload

--- src/createPools.js
+++ src/createPools.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Input, Button, Select } from 'semantic-ui-react'
+import { Input, Button, Select, Label } from 'semantic-ui-react'
 
 class CreatePools extends Component {
 
   state = {
     teamName: "",
+    value: 1,
     number: 3,
+    options: [],
+  }
+
+  componentWillMount(){
+    let optionss=[];
+    for(let i=1;i<=parseInt(this.state.number);i++){
+          optionss=[...optionss,{key: i, text: i, value: i}];
+    }
+    this.setState({
+      options: optionss,
+    })
   }
 
   handleOnChange = (e) => {
@@ -14,21 +26,23 @@ class CreatePools extends Component {
     })
   }
 
+  handleSelectChange = (event,data) => {
+    this.setState({
+      value: data.value,
+    })
+  }
+
   render() {
-    let options=[];
-    //zmienic potem state
-    for(let i=1;i<parseInt(this.state.number);i++){
-      console.log("Witam",i)
-          options=[...options,{key: i, text: i, value: i}];
-    }
+
 
     return (
       <div className = {"formContainer"}>
-      <Input type='text' placeholder='Podaj nazwę drużyny' onChange={this.handleOnChange} action>
-      <input />
-        <Select compact options={options} defaultValue='articles' />
-        <Button type='submit'>Search</Button>
-      </Input>
+      <Input type='text' placeholder='Podaj nazwę drużyny' onChange={this.handleOnChange} action />
+      <div>
+      <Label size={"big"}>Wybierz grupę</Label>
+      <Select compact onChange={ this.handleSelectChange} options={this.state.options} value={this.state.value} />
+      </div>
+      <Button onClick={this.handleOnClick}>Dodaj drużynę</Button>
       </div>
 
     );
