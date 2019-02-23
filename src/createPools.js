d3ab46a5ee353839f08da647dc66b93701aae8b3
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-23 16:57:18 +0100

    Add files via upload
    added creating groups

--- src/createPools.js
+++ src/createPools.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Input, Button, Select, Label } from 'semantic-ui-react'
+import Draw from './draw.js'
 
 class CreatePools extends Component {
 
@@ -8,16 +9,17 @@ class CreatePools extends Component {
     value: 1,
     number: 3,
     options: [],
+    draw1: [],
   }
 
   componentWillMount(){
     let optionss=[];
     for(let i=1;i<=parseInt(this.state.number);i++){
-          optionss=[...optionss,{key: i, text: i, value: i}];
+          optionss=[...optionss,{key: i, text: i, value: i, teams: []}];
     }
     this.setState({
       options: optionss,
-    })
+    });
   }
 
   handleOnChange = (e) => {
@@ -32,18 +34,43 @@ class CreatePools extends Component {
     })
   }
 
+  handleOnClick = () => {
+    if(this.state.teamName.length>0){
+      let team = this.state.teamName;
+      let option = this.state.options;
+      option[this.state.value-1].teams=[...option[this.state.value-1].teams,team];
+      console.log(option);
+      this.setState({
+        options: option,
+        teamName: "",
+        value: 1,
+      })
+    }
+  }
+
   render() {
 
+    const draws = this.state.options.map((el,index)=>{
+      return <Draw key={index} index={index+1} group={this.state.options[index].teams} name={"Koszyk"}/>
+    })
 
     return (
-      <div className = {"formContainer"}>
-      <Input type='text' placeholder='Podaj nazwę drużyny' onChange={this.handleOnChange} action />
-      <div>
-      <Label size={"big"}>Wybierz grupę</Label>
-      <Select compact onChange={ this.handleSelectChange} options={this.state.options} value={this.state.value} />
+      <div className={"createContainer"}>
+        <div className={"poolContainer"}>
+          {draws}
+        </div>
+
+        <div className = {"formContainer"}>
+          <Input type='text' placeholder='Podaj nazwę drużyny' onChange={this.handleOnChange} action value={this.state.teamName} />
+          <div>
+          <Label size={"big"}>Wybierz grupę</Label>
+          <Select compact onChange={ this.handleSelectChange} options={this.state.options} value={this.state.value} />
+          </div>
+          <Button onClick={this.handleOnClick}>Dodaj drużynę</Button>
+        </div>
       </div>
-      <Button onClick={this.handleOnClick}>Dodaj drużynę</Button>
-      </div>
+
+
 
     );
   }
